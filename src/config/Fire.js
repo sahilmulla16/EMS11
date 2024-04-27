e2677bc75b5358bc630eba8017ec61413e1ecbc9
sahilmulla16 <133255540+sahilmulla16@users.noreply.github.com>
2024-04-27 16:06:22 +0530

    Update Fire.js

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -1,22 +1,22 @@
-import firebase from 'firebase/app';
-require("firebase/database");
-require("firebase/auth");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const config = {
-  apiKey: "AIzaSyBbJjYiENW_khO-w4x1CTHdKI8EIWsvnRY",
-  authDomain: "employee-management-system-111.firebaseapp.com",
-  databaseURL: "https://employee-management-system-111-default-rtdb.firebaseio.com",
-  projectId: "employee-management-system-111",
-  storageBucket: "employee-management-system-111.appspot.com",
-  messagingSenderId: "702903087835",
-  appId: "1:702903087835:web:5c44d43be9f925a090d2fe",
-  measurementId: "G-KWGPCZ0ZN2"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBARs1tYteasvlCBfRrhVmPkBWlPQ05W30",
+  authDomain: "employee-management-system-121.firebaseapp.com",
+  databaseURL: "https://employee-management-system-121-default-rtdb.firebaseio.com",
+  projectId: "employee-management-system-121",
+  storageBucket: "employee-management-system-121.appspot.com",
+  messagingSenderId: "510911840652",
+  appId: "1:510911840652:web:b626a5aeb7e8d638ddd6a9",
+  measurementId: "G-F44ZDWMCP0"
 };
 
 // Initialize Firebase
-const fire = firebase.initializeApp(config);
-
-export const auth = firebase.auth;
-export const db = firebase.database();
-
-export default fire;
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
