4c8191b88c6cb80db94a588681bb9232468ebc17
sahilmulla16 <133255540+sahilmulla16@users.noreply.github.com>
2024-04-27 16:12:16 +0530

    Update Fire.js

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/app';
+require("firebase/database");
+require("firebase/auth");
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBARs1tYteasvlCBfRrhVmPkBWlPQ05W30",
   authDomain: "employee-management-system-121.firebaseapp.com",
@@ -18,5 +14,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const fire = firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth;
+export const db = firebase.database();
+
+export default fire;
